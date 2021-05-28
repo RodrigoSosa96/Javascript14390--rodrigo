0fff53cb3c4318c252e4b09996481ffbc34655e0
Rodrigo Sosa <rod2008@live.com.ar>
2021-05-28 05:21:35 -0300

    Entrega 2

--- Proyectos-finales/Entrega-02/js/main.js
+++ Proyectos-finales/Entrega-02/js/main.js
@@ -9,14 +9,13 @@ Estilos basicos, pero por ahora funcionales para esta entrega
 let idNuevo = 1//id base para nuevos articulos
 let idcheck = 100
 class articulo{
-    constructor( categoria, marca, features, precio, stock, descuento) {
+    constructor(datos) {
         this.id = idNuevo++;
-        this.categoria = categoria;
-        this.marca = marca;
-        this.features = features
-        this.precio = parseInt(precio);
-        this.stock = parseInt(stock);
-        this.iva = 0.21
+        this.categoria = datos.categoria;
+        this.marca = datos.marca;
+        this.features = datos.features
+        this.precio = parseInt(datos.precio);
+        this.stock = parseInt(datos.stock);
     }
     //comprobar stock
     vendido(){
@@ -27,7 +26,7 @@ class articulo{
         this.precio = this.precio - (this.precio * (descuento / 100))
     }
     sumarIva(){
-        this.precio = this.precio + (this.precio * this.iva);
+        this.precio = this.precio + (this.precio * 0.21);
     }
     restarDescuento(){
         this.precio = this.precio - (this.descuento * this.precio);
@@ -35,60 +34,53 @@ class articulo{
 
 }
 
-//Duda: cómo haría para agregar todos los objetos a una array?, o sea sin las propiedades, para ahorrar espacio al almacenarlo en el storage
-/*
-for (let e = 0; e < datos.length; e++) {
-    console.log(Object.values(datos[e]));
-}
-
-ejemplo = datos.map(a => [a.id, a.categoria, a.marca, a.features, a.precio, a.stock])
-*/
-
-
-//Chequea si hay determinada llave en el storage
+//-----Datos a Storage y luego por el generador para mantener articulos generados
+//Primero chequea si hay determinada llave en el storage
 function checkStorage (key) {
-    if (key in sessionStorage) {
+    if (key in localStorage) {
         console.log("si")
 
 
     }else {
         console.log("no")
-        makeStorage()
+        makeStorage(datos)
     }
 }
 checkStorage('listaProductos')
 
-
-
-//Luego puedo hacerlo mas dinamico para distintas llaves junto al checkStorage (creo)
-function makeStorage() {
-    datosJSON = JSON.stringify(datos)
-    sessionStorage.setItem("listaProductos", datosJSON)
+//Paso los datos al storage en caso de no estar ya guardado
+//Se reutiliza para actualizar datos
+function makeStorage(key) {
+    datosJSON = JSON.stringify(key)
+    localStorage.setItem("listaProductos", datosJSON)
 }
 
-const datosParse = JSON.parse(sessionStorage.getItem('listaProductos'))
-
-
-//Construyo array desde Storage
+//Parseo de datos
+const datosParse = JSON.parse(localStorage.getItem('listaProductos'))
 const datosArmados = []
 for(let e of datosParse) {
-    datosArmados.push(new articulo( e.categoria, e.marca, e.features, e.precio, e.stock))
+    datosArmados.push(new articulo(e))
 }
-console.table(datosArmados)
+console.table(datosArmados)//test
 
 
-
-
-
+///--------Generadores de HTML
+//Generar opciones en select
+function generarOpciones(){
+    let padre = document.getElementById("categoriaProducto");
+    let inner = "";
+    for (const elemento of categoria) {
+    inner += `<option value="${elemento}">${elemento}</option>`;
+    }
+    padre.innerHTML = inner;
+}
+generarOpciones()
+//Generador de articulos
 const pref = "productoID"
 let contenedorPadre = document.getElementById("listaProductos")
-
-
-
 for(let dato of datosArmados) {
     crearElemento(dato)
 }
-
 function crearElemento(dato){
     let nuevoElemento   = document.createElement("div")
     nuevoElemento.id    =pref + dato.id
@@ -99,7 +91,7 @@ function crearElemento(dato){
                                 <p>Características: ${dato.features}</p>
                                 <p>Precio: $ ${dato.precio}</p>
                                 <p id=stock${dato.id} >Stock: ${dato.stock} unidades</p>
-                                <button id="${dato.id}">Agregar</button>
+                                <button id=${dato.id}>Agregar</button>
                                 </div>
                                 `
     contenedorPadre.appendChild(nuevoElemento)
@@ -107,6 +99,8 @@ function crearElemento(dato){
     boton.onclick = () => {       //Con cada click se actualiza el stock y en caso de no haber más, se bloquea el botón.
         
         if(dato.stock >= 1) {
+            carrito(dato.id)
+            
             console.log("PRODUCTO VENDIDO" + dato.id);
             dato.vendido()  
             document.getElementById('stock'+dato.id).innerHTML = `Stock: ${dato.stock} unidades`
@@ -115,36 +109,80 @@ function crearElemento(dato){
         } else {
             console.log("No hay stock")
             document.getElementById(dato.id).disabled = true;
-
-
         }
 
     };
 }
-//despues de comprar ya hay menos articulos en la siguiente sesión con localstorage
-//paso a json y luego parseo todo de nuevo en cada sesión
 
-//carrito
-
-
-const formData = []
-//formulario nuevos elementos
-//document.getElementById("idForm").innerHTML = `Articulo numero: ${idcheck}`  
-let entradaForm = document.getElementById("formulario")
-entradaForm.onsubmit = (e) => {
-    e.preventDefault();
-    dataform = {
-        categoria: e.target.children[1].value,
-        marca: e.target.children[3].value,
-        features: e.target.children[5].value,
-        stock: e.target.children[7].value,
-        precio: e.target.children[9].value
+//Agregar elementos a los articulos
+function guardarTarea(){
+    let nuevoRegistro = {
+        id            : datosArmados.length + 1,
+        categoria     : document.getElementById("categoriaProducto").value,
+        marca         : document.getElementById("marcaProducto").value,
+        features      : document.getElementById("featuresProducto").value,
+        precio        : document.getElementById("precioProducto").value,
+        stock         : document.getElementById("stockProducto").value
     }
-    datosArmados.push(new articulo(dataform.categoria, dataform.marca, dataform.features, dataform.precio, dataform.stock))
-    //document.getElementById("idForm").innerHTML = `${idcheck}`  
+    datosArmados.push(new articulo(nuevoRegistro));
     let ultimoPush = datosArmados.length - 1
-    crearElemento(datosArmados[ultimoPush])//Si funciona pero da error a la consola, realmente funciona?
-    //sessionStorage.setItem('nuevo', (JSON.stringify(dataform)))
+    crearElemento(datosArmados[ultimoPush])
+    makeStorage(datosArmados)
+    //filtrarEvento();
 }
 
+
+
+
+//Generar Carrito(modificar)
+const carritoStorage = []
+function carrito(ids){
+    item = busqueda(ids)
+    console.log(item)
+    carritoStorage.push(item)
+    carritoJSON = JSON.stringify(carritoStorage)
+    let body = document.getElementById("tablaCarrito").children[1];
+    let inner = "";
+    for (const e of item) {
+        inner += `<tr><td>${e.categoria}</td><td>${e.marca}</td><td>${e.features}</td><td>${e.stock}</td><td>${e.precio}</td></tr>`;
+    }
+    body.innerHTML = inner;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function clearAll() {
+    localStorage.clear();
+    location.reload();
+}
+
+
+window.onload = () => {
+    // SECCION DE ASOCIACIÓN DE EVENTOS EN ONLOAD ASI NOS ASEGURAMOS DE ASOCIAL UNA VEZ QUE EL DOM ESTA LISTO
+    document.getElementById("submitBtn").onclick =  guardarTarea;
+    document.getElementById("resetBtn").onclick = clearAll;
+    document.getElementById("")
+}
+
+//Inicialización
+
+
+
+
+
+
+
+function busqueda(id) {
+    return datosArmados.find(producto => producto.id === id); 
+}
 
